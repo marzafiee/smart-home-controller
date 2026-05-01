61cf6877dded5eba71adaaf594161056667acf53
Inez Anne-Marie <eyra.agbenu@ashesi.edu.gh>
2026-05-01 20:37:32 +0000

    fixed formatting issues

--- smarthome_classes/observers/ConsoleDisplay.java
+++ smarthome_classes/observers/ConsoleDisplay.java
@@ -8,6 +8,8 @@ import smarthome_classes.devices.SmartDevice;
 public class ConsoleDisplay implements DeviceObserver {
     @Override
     public void update(SmartDevice device, String action) {
-        System.out.printf("%s%s %s%n", "  ⟶  " + device.getName(), action);
+        // Avoid printf format parsing on action text, especially when action
+        // contains percent signs like "brightness set to 60%".
+        System.out.println("  ->  " + device.getName() + " " + action);
     }
 }
